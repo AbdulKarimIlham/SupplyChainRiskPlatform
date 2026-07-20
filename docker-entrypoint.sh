91f243eeb4071c0a422e0fe3f52d1aefd0335b4d
rissoledelicious <rissoledelicious@gmail.com>
2026-07-21 01:57:36 +0700

    Run migrations asynchronously to enable instant web server boot for Railway health check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-php artisan config:clear || true
-php artisan migrate --force || true
-php artisan db:seed --force || true
+
+# Run migrations and seeding asynchronously in background so web server boots instantly
+(
+  sleep 2
+  php artisan config:clear || true
+  php artisan migrate --force || true
+  php artisan db:seed --force || true
+) &
+
+# Immediately execute web server as PID 1 to pass Railway health check instantly
 exec php -S 0.0.0.0:${PORT:-8080} -t public
